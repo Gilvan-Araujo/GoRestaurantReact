65a8a1957f694ba8eeda172edc82a41717ec054e
Gilvan <45008443+Gilvan-Araujo@users.noreply.github.com>
2021-06-08 13:32:16 -0300

    Add and setup PWA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,15 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import Head from 'next/head';
 
 import { theme } from '../styles/theme';
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
+      <Head>
+        <title>GoodFod</title>
+        <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no' />
+      </Head>
       <Component {...pageProps} />
     </ChakraProvider>
   )
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,10 +3,22 @@ import Document, { Head, Html, Main, NextScript } from 'next/document';
 export default class MyDocument extends Document {
     render() {
         return (
-            <Html>
+            <Html lang='en-US'>
                 <Head>
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
+
+                    <meta charSet='utf-8' />
+                    <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
+                    <meta name='description' content='A simple fake restaurant application' />
+                    <meta name='keywords' content='restaurant foods management dishes' />
+                    <link rel="manifest" href="/manifest.json" />
+
+                    <link rel="icon" href="favicon.svg" />
+                    <link rel="mask-icon" href="mask-icon.svg" color="#171923" />
+                    <link rel="apple-touch-icon" href="apple-touch-icon.png" />
+
+                    <meta name="theme-color" content="#171923" />
                 </Head>
                 <body>
                     <Main />
